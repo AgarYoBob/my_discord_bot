1f3f80bc7cb50f77a936e20a44b751b6d1e11c91
AgarYoBob <32961681+AgarYoBob@users.noreply.github.com>
2019-06-20 20:42:42 +0900

    Update User_Embed_up.py

--- User_Embed_up.py
+++ User_Embed_up.py
@@ -17,7 +17,7 @@ async def on_ready():
     print("----------")
 
 
-def makeEmbed(*, name=None, icon=None, colour=0x738ADB, values={}):
+def makeEmbed(*, name=None, icon=None, colour=0xDEADBF, values={}):
     '''Creates an embed messasge with specified inputs'''
 
     embedObj = discord.Embed(colour=colour)
@@ -49,6 +49,30 @@ async def on_message(message):
         embed.set_footer(text="2019_06_20")
         await me.send_message(message.channel, embed=embed)
 
+    if message.content == "/소통3":
+        embed = discord.Embed(title="#소통해요_3", description="더운 여름을 날려버릴 가장 좋은 것을 말해봐요!", color=0x738adb)
+        embed.set_footer(text="2019_06_21")
+        await me.send_message(message.channel, embed=embed)
+
+    if message.content == "/소통4":
+        embed = discord.Embed(title="#소통해요_4", description="인생에 꼭 담고싶은 가장 좋아하는 명언같은 것이 있나요?\n있다면 그것은 무엇인가요?", color=0x738adb)
+        embed.set_footer(text="2019_06_22")
+        await me.send_message(message.channel, embed=embed)
+
+    if message.content == "/소통5":
+        embed = discord.Embed(title="#소통해요_5", description="이 서버의 이름인 [들어오면 후회함]을 보면 어떤 생각이 드는 지 말해보아요!", color=0x738adb)
+        embed.set_footer(text="2019_06_23")
+        await me.send_message(message.channel, embed=embed)
+
+    if message.content == "/소통6":
+        embed = discord.Embed(title="#소통해요_6", description="망할 월요일이 돌아왔습니다!\n돌아온 월요일을 위해(?) 한 마디씩 남겨주세요!", color=0x738adb)
+        embed.set_footer(text="2019_06_24")
+
+    if message.content == "/소통7":
+        embed = discord.Embed(title="#소통해요_7", description="***강아지 vs 고양이*** | 본인이 가장 좋아하는 것은?", color=0x738adb)
+        embed.set_footer(text="2019_06_25")
+        await me.send_message(message.channel, embed=embed)
+
     if not message.content.startswith('/e '):
         return
 
@@ -60,7 +84,5 @@ async def on_message(message):
     await me.edit_message(message, '  ', embed=actualObj)
 
 
-
-
-token = "MTUwNTc3MjkzOTgxNTE1Nzc2.XP0JzQ.5Vlf3gH_PlNR7aLS0_NFFTrsVl4"
+token = os.environ["USER_TOKEN"]
 me.run(token, bot=False)
